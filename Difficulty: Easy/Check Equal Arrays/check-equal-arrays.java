26f5a4b68794f04ac7bd483d54b3ea70c46380ed
Vaibhav Kumar Thakur <110083399+VaibhavKT20@users.noreply.github.com>
2025-07-16 09:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    public static boolean checkEqual(int[] a, int[] b) {
+        // Your code here
+        if(a.length!=b.length){
+            return false;
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
