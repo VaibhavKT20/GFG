dc34dbaca79b6f7e75730938d859cd549aa0f0a9
Vaibhav Kumar Thakur <110083399+VaibhavKT20@users.noreply.github.com>
2025-09-13 12:00:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level Order in spiral form/level-order-in-spiral-form.java
@@ -0,0 +1,51 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<Integer> findSpiral(Node root) {
+        // code here
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        Queue<Node> q=new LinkedList<Node>();
+        Stack<Integer>s=new Stack<Integer>();
+        boolean reverse=true;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int count=q.size();
+            for(int i=0;i<count;i++){
+                Node curr=q.poll();
+                if(reverse){
+                    s.push(curr.data);
+                }
+                else{
+                    result.add(curr.data);
+                }
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
+            }
+            if(reverse){
+                while(!s.isEmpty()){
+                    result.add(s.pop());
+                }
+            }
+            reverse=!reverse;
+        }
+        return result;
+    }
+}
