f564fbf7e89bb7f606c2cc29cc47ddc47300d94a
Vaibhav Kumar Thakur <110083399+VaibhavKT20@users.noreply.github.com>
2025-10-09 17:26:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,25 +1,20 @@
 class Solution {
-    // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
         // code here
-        int n=arr.length;
-        int zero=0 ,one=0, two=0;
-        for(int i=0;i<n;i++){
+        int zero=0;
+        int one=0;
+        int two=0;
+        for(int i=0;i<arr.length;i++){
             if(arr[i]==0){
                 zero++;
             }
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==1){
+            else if(arr[i]==1){
                 one++;
             }
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==2){
+            else{
                 two++;
             }
         }
-        
         int index=0;
         for(int i=0;i<zero;i++){
             arr[index++]=0;
